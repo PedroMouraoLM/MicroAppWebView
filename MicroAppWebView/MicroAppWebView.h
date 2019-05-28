defd9fdd42a79351d67c3d4ffee9c2d40e51d76b
victoraliss0n <victoralissont5@gmail.com>
2019-05-28 00:38:45 -0300

    Initial Commit

---
+++ MicroAppWebView/MicroAppWebView.h
@@ -0,0 +1,19 @@
+//
+//  MicroAppWebView.h
+//  MicroAppWebView
+//
+//  Created by Pedro Albuquerque on 28/05/19.
+//  Copyright © 2019 Soluevo Solucoes Evolutivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MicroAppWebView.
+FOUNDATION_EXPORT double MicroAppWebViewVersionNumber;
+
+//! Project version string for MicroAppWebView.
+FOUNDATION_EXPORT const unsigned char MicroAppWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MicroAppWebView/PublicHeader.h>
+
+
