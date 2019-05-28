b6027093faf87eb84d7010bb158af62c9de7099d
victoraliss0n <victoralissont5@gmail.com>
2019-05-28 01:05:44 -0300

    commit initial

---
+++ MicroAppWebView/SLWebView.swift
@@ -0,0 +1,9 @@
+//
+//  SLWebView.swift
+//  MicroAppWebView
+//
+//  Created by Pedro Albuquerque on 28/05/19.
+//  Copyright © 2019 Soluevo Solucoes Evolutivas. All rights reserved.
+//
+
+import Foundation
